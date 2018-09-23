34fb26423d0b52c1c79223bde12bb159b607779a
DEBASHIS GOGOI <42699092+Deba04@users.noreply.github.com>
2018-09-23 19:22:09 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,56 @@
-X <- rbind(x_train, x_test)
-Y <- rbind(y_train, y_test)
-Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, Y, X)
-TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
-TidyData$code <- activities[TidyData$code, 2]
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
-FinalData <- TidyData %>%
-  group_by(subject, activity) %>%
-  summarise_all(funs(mean))
-write.table(FinalData, "FinalData.txt", row.name=FALSE)
+## run_analysis.R
+## Setting the scene
+library(data.table)
+library(dplyr)
+
+wd <- getwd()
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(URL, file.path(wd, "dataFiles.zip"))
+unzip(zipfile = "dataFiles.zip")
+
+x_train <- read.table("./train/X_train.txt", header = FALSE)
+y_train <- read.table("./train/y_train.txt", header = FALSE)
+subject_train <- read.table("./train/subject_train.txt", header = FALSE)
+
+x_test <- read.table("./test/X_test.txt", header = FALSE)
+y_test <- read.table("./test/y_test.txt", header = FALSE)
+subject_test <- read.table("./test/subject_test.txt", header = FALSE)
+
+features <- read.table("features.txt", header = FALSE)
+features <- as.data.table(features[,2]); names(features) <- "features"
+features$duplicate <- duplicated(features); features$appendix <- ifelse(features$duplicate == TRUE, "D", ""); features$paste <- paste(features$features, features$appendix, sep="")
+features$dduplicate <- duplicated(features$paste); features$aappendix <- ifelse(features$dduplicate == TRUE, "D", ""); features$ppaste <- paste(features$paste, features$aappendix, sep="")
+
+columns <- as.vector(features$ppaste)
+
+labels <- as.data.table(read.table("activity_labels.txt", header=FALSE))
+names(labels) <- c("ID", "Activity")
+
+## Merge the training and the test sets to create one data set.
+train_data <- cbind(subject_train, y_train, x_train)
+test_data <- cbind(subject_test, y_test, x_test)
+data <- as.data.table(rbind(train_data, test_data))
+
+## Extract only the measurements on the mean and standard deviation for each measurement.
+names(data) <- c("Subject", "Activity", columns)
+data <- select(data, Subject:Activity, matches("mean()"), matches("std()"))
+
+## Use descriptive activity names to name the activities in the data set.
+for (i in 1:nrow(labels)){
+      data$Activity <- replace(data$Activity, data$Activity == i, as.character(labels[i, Activity]))
+}
+
+## Appropriately label the data set with descriptive variable names.
+names(data) <- gsub("-", ".", names(data))
+names(data) <- gsub(",", ".", names(data))
+names(data) <- gsub("\\()", "", names(data))
+names(data) <- gsub("\\(", ".", names(data))
+names(data) <- gsub("\\)", "", names(data))
+names(data) <- gsub("tBody", "timeBody", names(data))
+names(data) <- gsub("tGrav", "timeGrav", names(data))
+names(data) <- gsub("fBody", "freqBody", names(data))
+
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+data2 <- group_by(data, Subject, Activity) %>% summarise_all(mean)
+
+print(data2)
